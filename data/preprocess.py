bb0f6e340100d6f19ca48b576b2793b6af77f7c4
JISUNG PARK <llllllllll@kakao.com>
2021-01-29 01:11:42 +0900

    feat: Normalize and augment image
    [-1, 1]로 정규화 및 augment 옵션에 따른 확장

--- data/preprocess.py
+++ data/preprocess.py
@@ -3,6 +3,7 @@
 # MIT License
 
 import os
+import random
 
 import tensorflow as tf
 
@@ -28,6 +29,35 @@ class Image_data:
         self.class_info_pickle = \
             os.path.join(self.text_path, 'class_info.pickle')
 
+    def image_processing(self, filename, captions, class_id=None):
+        x = tf.io.read_file(filename)
+        x_decode = tf.image.decode_jpeg(x, channels=self.channels,
+                                        dct_method='INTEGER_ACCURATE')
+        img = tf.image.resize(x_decode, [self.img_height, self.img_width])
+        img = tf.cast(img, tf.float32) / 127.5 - 1
+
+        if self.augment_flag:
+            augment_height_size = self.img_height + \
+                                  (30 if self.img_height == 256
+                                   else int(self.img_height * 0.1))
+            augment_width_size = self.img_width + \
+                                 (30 if self.img_width == 256
+                                  else int(self.img_width * 0.1))
+
+            seed = random.randint(0, 2 ** 31 - 1)
+            condition = tf.greater_equal(
+                tf.random.uniform(shape=[], minval=0.0, maxval=1.0),
+                0.5
+            )
+
+            img = tf.cond(pred=condition,
+                          true_fn=lambda: augmentation(
+                              img, augment_height_size,
+                              augment_width_size, seed
+                          ),
+                          false_fn=lambda: img)
+
+        return img, captions, class_id
 
 def augmentation(image, augment_height, augment_width, seed):
     ori_image_shape = tf.shape(image)
