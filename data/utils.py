11382ba96a0b1bdd2ce3729e5cadc6938c44ac4a
JISUNG PARK <llllllllll@kakao.com>
2021-01-30 01:25:10 +0900

    style: Move data/utils to utils/others

--- data/utils.py
+++ data/utils.py
@@ -21,3 +21,6 @@ def check_folder(log_dir):
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
     return log_dir
+
+def str2bool(x):
+    return x.lower() in ('true')
---
+++ utils/others.py
@@ -0,0 +1,10 @@
+# © 2021 지성. all rights reserved.
+# <llllllllll@kakao.com>
+# MIT License
+
+import os
+
+def check_folder(log_dir):
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+    return log_dir
