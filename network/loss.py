9c4b232114bb669263e6f69e911cd6bce2837459
JISUNG PARK <llllllllll@kakao.com>
2021-01-30 00:43:08 +0900

    feat: Define word-level correlation loss

--- network/loss.py
+++ network/loss.py
@@ -2,10 +2,11 @@
 # <llllllllll@kakao.com>
 # MIT License
 
-from network.utils import Relu
+from network.utils import Relu, func_attention, cosine_similarity
 
-from tensorflow import (nn, function, reduce_mean, square, math,
-                        zeros_like, ones_like)
+from tensorflow import (nn, function, reduce_mean, square, math, transpose,
+                        zeros_like, ones_like, expand_dims, matmul, tile,
+                        reshape, exp, reduce_sum)
 
 ##############################################################################
 # Loss Function
@@ -93,5 +94,51 @@ def caption_loss(cap_output, captions):
     loss = nn.softmax_cross_entropy_with_logits(logits=cap_output,
                                                 labels=captions)
     loss = reduce_mean(loss)
+
+    return loss
+
+def word_level_correlation_loss(img_feature, word_emb,
+                                gamma1=4.0, gamma2=5.0):
+    batch_size = img_feature.shape[0]
+    seq_len = word_emb.shape[1]
+    similar_list = []
+
+    for i in range(batch_size) :
+        context = expand_dims(img_feature[i], axis=0)
+        word = expand_dims(word_emb[i], axis=0)
+
+        weighted_context, attn = func_attention(context, word, gamma1)
+
+        aver_word = reduce_mean(word, axis=1, keepdims=True)
+
+        res_word = matmul(aver_word, word, transpose_b=True)
+        res_word_softmax = nn.softmax(res_word, axis=1)
+        res_word_softmax = tile(res_word_softmax,
+                                multiples=[1, weighted_context.shape[1], 1])
+
+        self_weighted_context = transpose(weighted_context * res_word_softmax,
+                                          perm=[0, 2, 1])
+
+        word = reshape(word, [seq_len, -1])
+        self_weighted_context = reshape(self_weighted_context, [seq_len, -1])
+
+        row_sim = cosine_similarity(word, self_weighted_context)
+
+        row_sim = exp(row_sim * gamma2)
+        row_sim = reduce_sum(row_sim)
+        row_sim = math.log(row_sim)
+
+        similar_list.append(row_sim)
+
+    word_match_loss = reduce_mean(
+        nn.sigmoid_cross_entropy_with_logits(logits=similar_list,
+                                             labels=ones_like(similar_list))
+    )
+    word_mismatch_loss = reduce_mean(
+        nn.sigmoid_cross_entropy_with_logits(logits=similar_list,
+                                             labels=zeros_like(similar_list))
+    )
+
+    loss = (word_match_loss + word_mismatch_loss) / 2.0
 
     return loss
