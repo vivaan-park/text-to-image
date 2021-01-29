5eb3d2f2979273e551c1350ff5a3fd5fa9e17691
JISUNG PARK <llllllllll@kakao.com>
2021-01-30 00:06:25 +0900

    feat: Define GAN generator loss

--- network/loss.py
+++ network/loss.py
@@ -56,3 +56,19 @@ def discriminator_loss(gan_type, real_logit, fake_logit):
             fake_loss = reduce_mean(Relu(1 + fake_logit))
 
     return real_loss, fake_loss
+
+@function
+def generator_loss(gan_type, fake_logit):
+    fake_loss = 0
+
+    if gan_type == 'lsgan':
+        fake_loss = reduce_mean(math.squared_difference(fake_logit, 1.0))
+    elif gan_type == 'gan':
+        fake_loss = reduce_mean(
+            nn.sigmoid_cross_entropy_with_logits(labels=ones_like(fake_logit),
+                                                 logits=fake_logit)
+        )
+    elif gan_type == 'hinge':
+        fake_loss = -reduce_mean(fake_logit)
+
+    return fake_loss
