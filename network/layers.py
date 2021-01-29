4d9d9ed0eca7d1a065386cf4de154a332b10e669
JISUNG PARK <llllllllll@kakao.com>
2021-01-29 12:23:22 +0900

    feat: Call convolutional layer

--- network/layers.py
+++ network/layers.py
@@ -6,6 +6,7 @@ from network.class_func import SpectralNormalization
 from utils.params import *
 
 from tensorflow.keras.layers import Layer, Conv2D
+from tensorflow import pad
 
 class Conv(Layer):
     def __init__(self, channels, kernel=3, stride=1, pad=0,
@@ -37,3 +38,27 @@ class Conv(Layer):
                 strides=self.stride, use_bias=self.use_bias,
                 name=self.name
             )
+
+    def call(self, x, training=None, mask=None):
+        if self.pad > 0:
+            h = x.shape[1]
+            if h % self.stride == 0:
+                pad = self.pad * 2
+            else:
+                pad = max(self.kernel - (h % self.stride), 0)
+
+            pad_top = pad // 2
+            pad_bottom = pad - pad_top
+            pad_left = pad // 2
+            pad_right = pad - pad_left
+
+            if self.pad_type == 'reflect':
+                x = pad(x, [[0, 0], [pad_top, pad_bottom],
+                            [pad_left, pad_right], [0, 0]], mode='REFLECT')
+            else:
+                x = pad(x, [[0, 0], [pad_top, pad_bottom],
+                            [pad_left, pad_right], [0, 0]])
+
+        x = self.conv(x)
+
+        return x
