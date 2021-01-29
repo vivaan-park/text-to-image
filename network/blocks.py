145215c1a635b275f94d8bc82835e28afb01a2da
JISUNG PARK <llllllllll@kakao.com>
2021-01-29 20:43:21 +0900

    feat: Call ResBlock

--- network/blocks.py
+++ network/blocks.py
@@ -3,9 +3,10 @@
 # MIT License
 
 from network.layers import Conv
-from network.utils import BatchNorm
+from network.utils import BatchNorm, GLU
 
 from tensorflow.keras.layers import Layer
+from tensorflow import name_scope
 
 class ResBlock(Layer):
     def __init__(self, channels, name='ResBlock'):
@@ -21,3 +22,16 @@ class ResBlock(Layer):
                            pad_type='reflect', use_bias=False,  name='conv_1')
         self.batch_norm_1 = BatchNorm(momentum=0.9, epsilon=1e-5,
                                       name='batch_norm_1')
+
+    def call(self, x_init, training=None, mask=None):
+        with name_scope(self.name):
+            with name_scope('res1'):
+                x = self.conv_0(x_init)
+                x = self.batch_norm_0(x, training=training)
+                x = GLU()(x)
+
+            with name_scope('res2'):
+                x = self.conv_1(x)
+                x = self.batch_norm_1(x, training=training)
+
+            return x + x_init
