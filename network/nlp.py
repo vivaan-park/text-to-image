1943981b9a7dee36080e7b8638779b8cb5d0b2c7
JISUNG PARK <llllllllll@kakao.com>
2021-01-30 00:22:04 +0900

    feat: Call various RNNs

--- network/nlp.py
+++ network/nlp.py
@@ -4,6 +4,7 @@
 
 from tensorflow.keras.layers import (Layer, LSTMCell, GRUCell, RNN,
                                      Bidirectional)
+from tensorflow import concat
 
 class VariousRNN(Layer):
     def __init__(self, n_hidden=128, n_layer=1, dropout_rate=0.5,
@@ -31,3 +32,31 @@ class VariousRNN(Layer):
         )
         if self.bidirectional:
             self.rnn = Bidirectional(self.rnn)
+
+    def call(self, x, training=None, mask=None):
+        if self.return_state:
+            if self.bidirectional:
+                if self.rnn_type == 'gru':
+                    output, forward_h, backward_h = \
+                        self.rnn(x, training=training)
+                elif self.rnn_type == 'lstm':
+                    output, forward_state, backward_state = \
+                        self.rnn(x, training=training)
+                    forward_h, backward_h = forward_state[0], backward_state[0]
+
+                sent_emb = concat([forward_h, backward_h], axis=-1)
+            else:
+                if self.rnn_type =='gru':
+                    output, forward_h = self.rnn(x, training=training)
+                elif self.rnn_type == 'lstm':
+                    output, forward_state = self.rnn(x, training=training)
+                    forward_h, _ = forward_state
+
+                sent_emb = forward_h
+        else:
+            output = self.rnn(x, training=training)
+            sent_emb = output[:, -1, :]
+
+        word_emb = output
+
+        return word_emb, sent_emb
