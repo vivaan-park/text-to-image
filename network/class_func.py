3604907cd88978d5c4aedd131b53b722b7ec8dd4
JISUNG PARK <llllllllll@kakao.com>
2021-01-29 12:06:48 +0900

    style: Rename layers to class_func

---
+++ network/class_func.py
@@ -0,0 +1,59 @@
+# © 2021 지성. all rights reserved.
+# <llllllllll@kakao.com>
+# MIT License
+
+import tensorflow as tf
+
+class SpectralNormalization(tf.keras.layers.Wrapper):
+    def __init__(self, layer, iteration=1, eps=1e-12, training=True, **kwargs):
+        self.iteration = iteration
+        self.eps = eps
+        self.do_power_iteration = training
+        if not isinstance(layer, tf.keras.layers.Layer):
+            raise ValueError(
+                'Please initialize `TimeDistributed` layer with a '
+                f'`Layer` instance. You passed: {layer}')
+        super(SpectralNormalization, self).__init__(layer, **kwargs)
+
+    def build(self, input_shape=None):
+        self.layer.build(input_shape)
+
+        self.w = self.layer.kernel
+        self.w_shape = self.w.shape.as_list()
+
+        self.u = self.add_weight(
+            shape=(1, self.w_shape[-1]),
+            initializer=tf.initializers.TruncatedNormal(stddev=0.02),
+            trainable=False,
+            name=self.name + '_u',
+            dtype=tf.float32, aggregation=tf.VariableAggregation.ONLY_FIRST_REPLICA
+        )
+
+        super(SpectralNormalization, self).build()
+
+    def call(self, inputs, training=None, mask=None):
+        self.update_weights()
+        output = self.layer(inputs)
+        return output
+
+    def update_weights(self):
+        w_reshaped = tf.reshape(self.w, [-1, self.w_shape[-1]])
+
+        u_hat = self.u
+        v_hat = None
+
+        if self.do_power_iteration:
+            for _ in range(self.iteration):
+                v_ = tf.matmul(u_hat, tf.transpose(w_reshaped))
+                v_hat = v_ / (tf.reduce_sum(v_ ** 2) ** 0.5 + self.eps)
+
+                u_ = tf.matmul(v_hat, w_reshaped)
+                u_hat = u_ / (tf.reduce_sum(u_ ** 2) ** 0.5 + self.eps)
+
+        sigma = tf.matmul(tf.matmul(v_hat, w_reshaped), tf.transpose(u_hat))
+        self.u.assign(u_hat)
+
+        self.layer.kernel = self.w / sigma
+
+    def restore_weights(self):
+        self.layer.kernel = self.w
