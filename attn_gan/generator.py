96dba511a6e42854e7fd95a30ce236d9389ac00f
JISUNG PARK <llllllllll@kakao.com>
2021-01-31 00:09:54 +0900

    feat: Initialize RNN encoder

--- attn_gan/generator.py
+++ attn_gan/generator.py
@@ -7,6 +7,7 @@ from tensorflow.keras.applications.inception_v3 import preprocess_input, Incepti
 from tensorflow import image
 
 from network.layers import Conv, FullyConnected
+from network.nlp import VariousRNN
 
 class CnnEncoder(Model):
     def __init__(self, embed_dim, name='CnnEncoder'):
@@ -42,3 +43,21 @@ class CnnEncoder(Model):
         code = self.emb_code(code)
 
         return feature, code
+
+class RnnEncoder(Model):
+    def __init__(self, n_words, embed_dim=256, drop_rate=0.5,
+                 n_hidden=128, n_layer=1, bidirectional=True,
+                 rnn_type='lstm', name='RnnEncoder'):
+        super(RnnEncoder, self).__init__(name=name)
+        self.n_words = n_words
+        self.embed_dim = embed_dim
+        self.drop_rate = drop_rate
+        self.n_hidden = n_hidden
+        self.n_layer = n_layer
+        self.bidirectional = bidirectional
+        self.rnn_type = rnn_type
+
+        self.model = self.architecture()
+        self.rnn = VariousRNN(self.n_hidden, self.n_layer, self.drop_rate,
+                              self.bidirectional, rnn_type=self.rnn_type,
+                              name=self.rnn_type + '_rnn')
