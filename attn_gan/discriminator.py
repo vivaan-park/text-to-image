db7242c8094c149eac9ed691ea1fa20c258c0714
JISUNG PARK <llllllllll@kakao.com>
2021-01-31 00:51:38 +0900

    feat: Define Discriminator 64x64 architecture

--- attn_gan/discriminator.py
+++ attn_gan/discriminator.py
@@ -3,8 +3,10 @@
 # MIT License
 
 from tensorflow.keras.layers import Layer
+from tensorflow.keras import Sequential
 
 from network.layers import Conv
+from network.utils import Leaky_Relu, BatchNorm
 
 class Discriminator_64(Layer):
     def __init__(self, channels, name='Discriminator_64'):
@@ -16,3 +18,30 @@ class Discriminator_64(Layer):
         self.cond_logit_conv = Conv(channels=1, kernel=4, stride=4,
                                     use_bias=True, name='cond_d_logit')
         self.model, self.code_block = self.architecture()
+
+    def architecture(self):
+        model = []
+        model += [Conv(self.channels, kernel=4, stride=2, pad=1,
+                       pad_type='reflect', use_bias=False, name='convv')]
+        model += [Leaky_Relu(alpha=0.2)]
+
+        for i in range(3):
+            model += [Conv(self.channels * 2, kernel=4, stride=2, pad=1,
+                           pad_type='reflect', use_bias=False,
+                           name='conv_' + str(i))]
+            model += [BatchNorm(name=f'batch_norm_{str(i)}')]
+            model += [Leaky_Relu(alpha=0.2)]
+
+            self.channels = self.channels * 2
+
+        model = Sequential(model)
+
+        code_block = []
+        code_block += [Conv(self.channels, kernel=3, stride=1, pad=1,
+                            pad_type='reflect', use_bias=False,
+                            name='conv_code')]
+        code_block += [BatchNorm(name='batch_norm_code')]
+        code_block += [Leaky_Relu(alpha=0.2)]
+        code_block = Sequential(code_block)
+
+        return model, code_block
